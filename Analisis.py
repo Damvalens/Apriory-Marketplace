ac8867a1ea73dd0e9410855414f52cad8062d8e9
Damvalens <celso.abente16@gmail.com>
2023-09-18 09:19:54 -0400

    SE AÑADIO LA BASE DE SQL

--- Analisis.py
+++ Analisis.py
@@ -1,6 +1,68 @@
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
+from sqlalchemy import create_engine
+
+# Establecer la cadena de conexión a la base de datos
+server = '10.51.80.111,1433'
+database = 'pos_central'
+username = 'sa'
+password = 'Market2023'
+connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
+
+# Crear el motor de SQLAlchemy
+engine = create_engine(connection_string)
+
+# Establecer la consulta SQL que deseas ejecutar
+query = """
+select  CONCAT(
+    RIGHT(CAST(vd.zeta AS VARCHAR(50)), 50), '00',
+    RIGHT(CAST(vp.nro_caja AS VARCHAR(50)), 50), '00',
+    RIGHT(CAST(vp.nro_ticket AS VARCHAR(50)), 50),'00') AS Nro_transaccion,
+convert(date,vp.fecha) as FECHA,
+vd.zeta, vp.nro_caja,
+vp.nro_ticket,
+vd.codigo,
+convert(bigint, vd.unidades) as cantidad, p.descripcion_producto,
+cast((p.PRECIO_COSTO)*1.1 as int ) as PRECIO_COSTO,
+convert(bigint,vd.precio_final) as PRECIO_UNI,
+--CAST(vd.precio_final * CONVERT(BIGINT, vd.unidades)AS INT) AS PRECIO_SIN_DESC,
+convert(bigint, vd.unidades)  * CONVERT(int,vd.precio_final_desc) AS Total,
+convert(int,vp.desc_global) as Porc_desc,
+cast((cast(vd.precio_final as bigint) * convert(float,vp.desc_global)/100) as int) as Desc_Aplicado,
+convert(bigint, vd.unidades)  * CONVERT(int,vd.precio_final_desc)-cast((p.PRECIO_COSTO)*1.1 as int ) AS UTILIDAD,
+vp.documento, 
+vp.nombre_cliente, 
+pr.NOMBRE_PROVEEDOR, 
+s.Cod_seccion, 
+ss.Cod_sub_seccion,
+s.Secciones,
+ss.Sub_secciones,
+g.DESCRIPCION_GRUPO,  
+d.DESCRIPCION_CATE,
+f.DESCRIPCION_SUB_CATE
+from ventas_pos vp
+join ventas_det_pos vd on vp.zeta = vd.zeta and vp.nro_caja = vd.nro_caja and vp.nro_ticket = vd.nro_ticket
+join pegasus.DBO.productos p on vd.codigo = p.codigo
+LEFT join pegasus.DBO.Seccion S on (p.Cod_seccion = s.Cod_seccion)
+LEFT JOIN pegasus.DBO.Sub_seccion ss ON (p.Cod_sub_seccion = ss.Cod_sub_seccion)
+LEFT JOIN pegasus.DBO.GRUPO g ON (p.COD_GRUPO = g.COD_GRUPO)
+LEFT join pegasus.DBO.CATEGORIAS d on (p.COD_CATEGORIA = d.COD_CATEGORIA)
+LEFT JOIN pegasus.DBO.SUB_CATEGORIAS F ON (p.COD_SUB_CATEGORIA = f.COD_SUB_CATEGORIA)
+LEFT JOIN pegasus.DBO.proveedores pr ON (p.cod_proveedor = pr.cod_proveedor)
+WHERE convert(date,vp.fecha) BETWEEN '2023-08-01' AND '2023-08-31'
+
+group by vp.nro_caja, vp.nro_ticket, vd.codigo, vd.unidades, p.descripcion_producto,vp.desc_global,p.PRECIO_COSTO,vd.precio_final_desc,vp.fecha,
+vd.precio_final,  vp.documento, vp.nombre_cliente, pr.NOMBRE_PROVEEDOR,s.Cod_seccion, ss.Cod_sub_seccion,
+g.DESCRIPCION_GRUPO,  d.DESCRIPCION_CATE, f.DESCRIPCION_SUB_CATE,vd.zeta,s.Secciones,ss.Sub_secciones"""
+
+# Ejecutar la consulta y cargar los resultados en un DataFrame de Pandas
+df = pd.read_sql(query, engine)
+
+# Exportar el DataFrame a un archivo Excel
+df.to_excel('output.xlsx', index=False)
+
+
 import xlsxwriter
 # Carga los datos desde un archivo de Excel
 data = pd.read_excel('output.xlsx')
@@ -216,7 +278,7 @@ for col in data_bin.columns:
 output_unique_df = pd.DataFrame(output_data_unique, columns=['SECCION', 'TRANSACCIONES ÚNICAS'])
 #####################
 # Guarda los resultados en un archivo Excel
-with pd.ExcelWriter('resultado_Secciones.xlsx') as writer:
+with pd.ExcelWriter('prueba sql.xlsx') as writer:
     frequent_itemsets.to_excel(writer, sheet_name='Frecuentes', index=False)
     rules.to_excel(writer, sheet_name='Reglas', index=False)
     pd.DataFrame(output_data, columns=['Antecedente', 'Consecuente', 'Número de transacciones']).to_excel(writer, sheet_name='Transacciones', index=False)
